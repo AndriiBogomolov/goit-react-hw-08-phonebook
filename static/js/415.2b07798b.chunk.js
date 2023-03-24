13eeb43419111a6cef31cbfe125ff0f9c7183136
AndriiBogomolov <97605054+AndriiBogomolov@users.noreply.github.com>
2023-03-24 16:37:42 +0000

    Deploying to gh-pages from @ AndriiBogomolov/goit-react-hw-08-phonebook@d5f219fd28eb8f01d09e8ad4bfa21606938859d7 🚀

---
+++ static/js/415.2b07798b.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[415],{5415:function(e,t,n){n.r(t),n.d(t,{default:function(){return o}});var r=n(184),i={container:{display:"flex",alignItems:"center",justifyContent:"center",minHeight:"calc(70vh - 60px)"},title:{fontWeight:500,fontSize:48,textAlign:"center",color:"rgb(19, 13, 8)"}};function o(){return(0,r.jsx)("div",{style:i.container,children:(0,r.jsx)("h1",{style:i.title,children:"Your own phonebook"})})}}}]);
+//# sourceMappingURL=415.2b07798b.chunk.js.map
